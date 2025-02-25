d962be51e19a93086f994b42f759e7ddf17ca1ea
Alexia-Gomez <alexiaareg_23@alu.uabcs.mx>
2025-02-25 05:16:50 -0700

    Unidad 2 - Ejercicio 19 (LOGIN.2)

--- TrabajoProg/src/aplication/Ventana.java
+++ TrabajoProg/src/aplication/Ventana.java
@@ -1,11 +1,18 @@
 package aplication;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -17,6 +24,7 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 public class Ventana extends JFrame{
 
@@ -37,97 +45,184 @@ public class Ventana extends JFrame{
 		this.setMinimumSize(new Dimension(400,400));
 		this.setMaximumSize(new Dimension(600,600));
 		
-		//this.add(this.login());
+		this.add(this.login());
 		//this.add(this.registro());
-		this.add(this.usuarios());
+		//this.add(this.usuarios());
 		this.repaint();
 	
 	}
 	
-	Font etiquetas = new Font("Nunito", Font.BOLD, 15);
+	Font etiquetas2 = new Font("Nunito", Font.BOLD, 15); 
+	Font etiquetas3 = new Font("Nunito", Font.PLAIN, 14); 
 	
 	public JPanel login() {
 		
 		JPanel login = new JPanel();
 		login.setLocation(0,0);
-		login.setSize(500,500);
+		login.setSize(700,600);
 		login.setOpaque(true);
 		login.setBackground(Color.WHITE);
 		login.setVisible(true);
 		login.setLayout(null);
 		
 		JLabel etiqueta1 = new JLabel("Iniciar Sesión");
-		etiqueta1.setBounds(150, 60, 170, 40);
+		etiqueta1.setBounds(245, 120, 200, 40);
 		etiqueta1.setForeground(new Color(10, 73, 143));
 		etiqueta1.setBackground(Color.WHITE);
 		etiqueta1.setOpaque(true);
 		etiqueta1.setHorizontalAlignment(JLabel.CENTER);
-		etiqueta1.setFont(new Font("Nunito", Font.BOLD, 25));
+		etiqueta1.setFont(new Font("Nunito", Font.BOLD, 30));
 		login.add(etiqueta1);
 		
+		
+		
+		//ICONO USUARIO
+		ImageIcon usuario = new ImageIcon("C:\\Users\\alexi\\OneDrive\\Imágenes\\usuario1.png");
+		JLabel usuario_icon = new JLabel();
+		usuario_icon.setBounds(150, 210, 40, 40);
+		usuario_icon.setIcon(new ImageIcon(usuario.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
+		login.add(usuario_icon);
+		
+		//USUARIO
 		JLabel etiqueta2 = new JLabel("Usuario:");
-		etiqueta2.setBounds(90, 120, 160, 30);
+		etiqueta2.setBounds(190, 180, 160, 30);
 		etiqueta2.setBackground(Color.WHITE);
+		etiqueta2.setForeground(new Color(4, 83, 125));
 		etiqueta2.setOpaque(true);
-		etiqueta2.setFont(etiquetas);
+		etiqueta2.setFont(etiquetas2);
 		login.add(etiqueta2);
 		
-		JTextField user = new JTextField();
-		user.setBounds(90, 155, 280, 30);
+		JTextField user = new JTextField();	
+		user.setBounds(190, 215, 330, 30);
 		user.setBackground(new Color(237, 237, 237 ));
 		user.setOpaque(true);
-		user.setFont(etiquetas);
+		user.setFont(etiquetas3);
 		login.add(user);
 		
+		
+		
+		//ICONO CONTRASEÑA
+		ImageIcon contraseña = new ImageIcon("C:\\Users\\alexi\\OneDrive\\Imágenes\\contraseña.png");
+		JLabel contraseña_icon = new JLabel();
+		contraseña_icon.setBounds(150, 290, 40, 40);
+		contraseña_icon.setIcon(new ImageIcon(contraseña.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
+		login.add(contraseña_icon);
+		
+		//CONTRASEÑA
 		JLabel etiqueta3 = new JLabel("Contraseña:");
-		etiqueta3.setBounds(90, 200, 160, 30);
+		etiqueta3.setBounds(190, 260, 160, 30);
 		etiqueta3.setBackground(Color.WHITE);
+		etiqueta3.setForeground(new Color(4, 83, 125));
 		etiqueta3.setOpaque(true);
-		etiqueta3.setFont(etiquetas);
+		etiqueta3.setFont(etiquetas2);
 		login.add(etiqueta3);
 		
 		JPasswordField pass = new JPasswordField();
-		pass.setBounds(90, 235, 280, 30);
+		pass.setBounds(190, 295, 330, 30);
 		pass.setBackground(new Color(237, 237, 237 ));
 		pass.setOpaque(true);
-		pass.setFont(new Font("Nunito", Font.ITALIC, 15));
+		pass.setFont(etiquetas3);
 		login.add(pass);
 		
+		
+		
+		//RECORDAR CONTRASEÑA
 		JCheckBox box = new JCheckBox();
-		box.setBounds(90, 278, 30, 30);
+		box.setBounds(190, 330, 25, 30);
 		box.setBackground(Color.WHITE);
 		box.setOpaque(true);
 		login.add(box);
-		
+				
 		JLabel etiqueta4 = new JLabel("Recuérdame");
-		etiqueta4.setBounds(120, 278, 120, 30);
+		etiqueta4.setBounds(215, 330, 120, 30);
 		etiqueta4.setBackground(Color.WHITE);
+		etiqueta4.setForeground(new Color(4, 83, 125));
 		etiqueta4.setOpaque(true);
 		etiqueta4.setFont(new Font("Nunito", Font.PLAIN, 12));
 		login.add(etiqueta4);
-		
-		JButton btn = new JButton("Acceder");
-		btn.setBounds(270, 280, 100, 30);
-		btn.setBackground(new Color(10, 73, 143) );
-		btn.setForeground(Color.white);
-		btn.setOpaque(true);
-		btn.setFont(new Font("Nunito", Font.BOLD, 15));
-		login.add(btn);
-		
-		
+				
+	
+				
+		//RECUPERAR CONTRASEÑA
 		JLabel etiqueta5 = new JLabel("¿Has perdido tu contraseña?");
-		etiqueta5.setBounds(90, 325, 160, 30);
+		etiqueta5.setBounds(360, 330, 160, 30);
 		etiqueta5.setBackground(Color.WHITE);
+		etiqueta5.setForeground(new Color(4, 83, 125));
 		etiqueta5.setOpaque(true);
 		etiqueta5.setFont(new Font("Nunito", Font.ITALIC, 12));
 		login.add(etiqueta5);
+		
+		
+				
+		//BOTON ACCEDER
+		JButton btn_a = new JButton("Acceder");
+		btn_a.setBounds(370, 390, 150, 35);
+		btn_a.setBackground(new Color(10, 73, 143) );
+		btn_a.setForeground(Color.white);
+		btn_a.setOpaque(true);
+		btn_a.setFont(new Font("Nunito", Font.BOLD, 15));
+		login.add(btn_a);
+		
+		//BOTON CREAR
+		JButton btn_b = new JButton("Crear cuenta");
+		btn_b.setBounds(190, 390, 150, 35);
+		btn_b.setBackground(Color.white);
+		btn_b.setForeground(new Color(4, 83, 125));
+		btn_b.setOpaque(true);
+		btn_b.setFont(new Font("Nunito", Font.BOLD, 15));
+		login.add(btn_b);
+		
+		//HOVER
+		btn_a.addMouseListener(new MouseAdapter(){
+			
+			public void mouseEntered(MouseEvent e) {
+				btn_a.setBackground(new Color(0, 49, 74));
+			}
+			
+			public void mouseExited(MouseEvent e) {
+				btn_a.setBackground(new Color(4, 83, 125));
+			}
+		});
+		
+		btn_b.addMouseListener(new MouseAdapter(){
+			
+			public void mouseEntered(MouseEvent e) {
+				btn_b.setForeground(Color.white);
+				btn_b.setBackground(new Color(0, 49, 74));
+			}
+			
+			public void mouseExited(MouseEvent e) {
+				btn_b.setBackground(Color.WHITE);
+				btn_b.setForeground(new Color(4, 83, 125));
+			}
+		});
+		
+		
+		
+		
+		
+		//FONDO1
+		JLabel fondo1 = new JLabel();
+		fondo1.setBounds(100, 80, 480, 400);
+		fondo1.setBackground(Color.white);
+		fondo1.setOpaque(true);
+		login.add(fondo1);
+		
+		
+		//FONDO
+		ImageIcon fondo = new ImageIcon("C:\\Users\\alexi\\OneDrive\\Imágenes\\fondo.png");
+		JLabel fondo_img = new JLabel();
+		fondo_img.setBounds(0, 0, 700, 600);
+		fondo_img.setIcon(fondo);
+		//fondo_img.setIcon(new ImageIcon(fondo.getImage().getScaledInstance(700, 300, Image.SCALE_SMOOTH)));
+		login.add(fondo_img);
 		
 	
 		login.revalidate();
 		return login;
 		
 	}
-	
+/*	
 	public JPanel registro() {
 		JPanel registro = new JPanel();
 		registro.setLocation(500,0);
@@ -372,4 +467,7 @@ public class Ventana extends JFrame{
 		return usuarios;
 		
 	}
+	
+	*/
+	
 }
