c885b58c762f8a39f7eee0b81644cf4650070e17
Alexia-Gomez <alexiaareg_23@alu.uabcs.mx>
2025-03-20 15:59:07 -0700

    Unidad 3 _ Ejercicio 14 (Tic Tac Toe con marcador.2)

--- TrabajoProg/src/aplication/TicTacToe.java
+++ TrabajoProg/src/aplication/TicTacToe.java
@@ -64,7 +64,10 @@ public class TicTacToe {
 	private JLabel lblNewLabel_1;
 	private JPanel panel_2;
 	private JButton btnNewButton_9;
-	
+	private int contX=0;
+	private int contO=0;
+	String jugador="";
+	String ganador="";
 	
 	
 	private void initialize() {
@@ -80,21 +83,19 @@ public class TicTacToe {
 		frame.getContentPane().add(panel);
 		panel.setLayout(new GridLayout(3, 3, 0, 0));
 		
-		//usuario_icon.setIcon(new ImageIcon(usuario.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
-		
 		ImageIcon roll=new ImageIcon(TicTacToe.class.getResource("/aplication/cinnamonroll.png"));
 		ImageIcon melody=new ImageIcon(TicTacToe.class.getResource("/aplication/melody2.png"));
 		
 		
 		btnNewButton = new JButton("");
 		btnNewButton.setBackground(new Color(249, 231, 247));
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -116,13 +117,13 @@ public class TicTacToe {
 		
 		btnNewButton_2 = new JButton("");
 		btnNewButton_2.setBackground(new Color(249, 231, 247));
-		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_2.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -142,13 +143,13 @@ public class TicTacToe {
 		
 		btnNewButton_4 = new JButton("");
 		btnNewButton_4.setBackground(new Color(249, 231, 247));
-		btnNewButton_4.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_4.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_4.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -170,13 +171,13 @@ public class TicTacToe {
 		
 		btnNewButton_6 = new JButton("");
 		btnNewButton_6.setBackground(new Color(249, 231, 247));
-		btnNewButton_6.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_6.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_6.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -196,14 +197,14 @@ public class TicTacToe {
 		
 		btnNewButton_1 = new JButton("");
 		btnNewButton_1.setBackground(new Color(249, 231, 247));
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 0));
 		
 		btnNewButton_1.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -224,13 +225,13 @@ public class TicTacToe {
 		
 		btnNewButton_3 = new JButton("");
 		btnNewButton_3.setBackground(new Color(249, 231, 247));
-		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_3.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -252,13 +253,13 @@ public class TicTacToe {
 		
 		btnNewButton_5 = new JButton("");
 		btnNewButton_5.setBackground(new Color(249, 231, 247));
-		btnNewButton_5.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_5.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_5.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -279,13 +280,13 @@ public class TicTacToe {
 		
 		btnNewButton_7 = new JButton("");
 		btnNewButton_7.setBackground(new Color(249, 231, 247));
-		btnNewButton_7.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_7.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_7.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
@@ -306,17 +307,18 @@ public class TicTacToe {
 		
 		btnNewButton_8 = new JButton("");
 		btnNewButton_8.setBackground(new Color(249, 231, 247));
-		btnNewButton_8.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnNewButton_8.setFont(new Font("Tahoma", Font.BOLD, 0));
 		btnNewButton_8.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String jugador = "X";
+				jugador = "X";
 				if(turno) {
 					turno=false;
 					jugador="X";
 					btnNewButton_8.setIcon(new ImageIcon(roll.getImage().getScaledInstance(195,180, Image.SCALE_SMOOTH)));
+					
 				}else {
 					turno=true;
 					jugador="O";
@@ -350,6 +352,10 @@ public class TicTacToe {
 		btnNewButton_9.setBackground(new Color(247, 214, 225));
 		btnNewButton_9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				ganador="";
+				lblNewLabel.setText("X: ");
+				lblNewLabel_1.setText("O: ");
+				reinicio();
 			}
 		});
 		btnNewButton_9.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -360,40 +366,113 @@ public class TicTacToe {
 	
 	public void verificar() {
 		
-		if((btnNewButton.getText().equals(btnNewButton_2.getText()))&&(btnNewButton_2.getText().equals(btnNewButton_4.getText()))&&!(btnNewButton_4.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null, "Ganador: "+btnNewButton.getText()); 
+		if((btnNewButton.getText().equals(btnNewButton_2.getText()))&&(btnNewButton_2.getText().equals(btnNewButton_4.getText()))&&!(btnNewButton_4.getText().equals(""))) { 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton.getText());
+			ganador=btnNewButton.getText();
 		}
 		
 		if((btnNewButton_6.getText().equals(btnNewButton_1.getText()))&&(btnNewButton_1.getText().equals(btnNewButton_3.getText()))&&!(btnNewButton_3.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null, "Ganador: "+btnNewButton_6.getText()); 		
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton_6.getText()); 	
+			ganador=btnNewButton_6.getText();
 		}
 		
 		if((btnNewButton_5.getText().equals(btnNewButton_7.getText()))&&(btnNewButton_7.getText().equals(btnNewButton_8.getText()))&&!(btnNewButton_8.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null, "Ganador: "+btnNewButton_5.getText()); 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton_5.getText()); 
+			ganador=btnNewButton_5.getText();
 		}
 		
 		//
 		if((btnNewButton.getText().equals(btnNewButton_6.getText()))&&(btnNewButton_6.getText().equals(btnNewButton_5.getText()))&&!(btnNewButton_5.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null,"Ganador: "+btnNewButton.getText()); 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton.getText());
+			ganador=btnNewButton.getText();
 		}
 		
 		if((btnNewButton_2.getText().equals(btnNewButton_1.getText()))&&(btnNewButton_1.getText().equals(btnNewButton_7.getText()))&&!(btnNewButton_7.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null,"Ganador: "+btnNewButton_2.getText()); 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton_2.getText()); 
+			ganador=btnNewButton_2.getText();
 		}
 		
 		if((btnNewButton_4.getText().equals(btnNewButton_3.getText()))&&(btnNewButton_3.getText().equals(btnNewButton_8.getText()))&&!(btnNewButton_8.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null,"Ganador: "+btnNewButton_4.getText()); 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton_4.getText()); 
+			ganador=btnNewButton_4.getText();
 		}
 		
 		//
 		if((btnNewButton.getText().equals(btnNewButton_1.getText()))&&(btnNewButton_1.getText().equals(btnNewButton_8.getText()))&&!(btnNewButton_8.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null, "Ganador: "+btnNewButton.getText()); 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton.getText());
+			ganador=btnNewButton.getText();
 		}
 		
 		if((btnNewButton_4.getText().equals(btnNewButton_1.getText()))&&(btnNewButton_1.getText().equals(btnNewButton_5.getText()))&&!(btnNewButton_5.getText().equals(""))) {
-			JOptionPane.showConfirmDialog(null, "Ganador: "+btnNewButton_4.getText()); 
+			JOptionPane.showMessageDialog(null,"Ganador: "+btnNewButton_4.getText());
+			ganador=btnNewButton_4.getText();
 		}
+		
+		
+		
+		if(ganador.equals("X")) {
+			contX++;
+			lblNewLabel.setText("X: "+ contX);
+			
+			
+		}else if(ganador.equals("O")) {
+			contO++;
+			lblNewLabel_1.setText("O: "+ contO);
+			
+		}
+		
+		empate(jugador);
+		
 	}
+	
+		public void empate(String ganador){
+			if((!btnNewButton.getText().isEmpty())&&(!btnNewButton_1.getText().isEmpty())&&(!btnNewButton_2.getText().isEmpty())&&(!btnNewButton_3.getText().isEmpty())&&
+					(!btnNewButton_4.getText().isEmpty())&&(!btnNewButton_5.getText().isEmpty())&&(!btnNewButton_6.getText().isEmpty()&&!btnNewButton_7.getText().isEmpty())&&(!btnNewButton_8.getText().isEmpty())) {
+				JOptionPane.showMessageDialog(null, "Empate");
+				reinicio();
+			}
+	}
+		
+		public void reinicio() {
+			
+			turno=true;
+			
+			btnNewButton.setText("");
+			btnNewButton.setIcon(null);
+			btnNewButton.setEnabled(true);
+			
+			btnNewButton_1.setText("");
+			btnNewButton_1.setIcon(null);
+			btnNewButton_1.setEnabled(true);
+			
+			btnNewButton_2.setText("");
+			btnNewButton_2.setIcon(null);
+			btnNewButton_2.setEnabled(true);
+			
+			btnNewButton_3.setText("");
+			btnNewButton_3.setIcon(null);
+			btnNewButton_3.setEnabled(true);
+			
+			btnNewButton_4.setText("");
+			btnNewButton_4.setIcon(null);
+			btnNewButton_4.setEnabled(true);
+			
+			btnNewButton_5.setText("");
+			btnNewButton_5.setIcon(null);
+			btnNewButton_5.setEnabled(true);
+			
+			btnNewButton_6.setText("");
+			btnNewButton_6.setIcon(null);
+			btnNewButton_6.setEnabled(true);
+			
+			btnNewButton_7.setText("");
+			btnNewButton_7.setIcon(null);
+			btnNewButton_7.setEnabled(true);
+			
+			btnNewButton_8.setText("");
+			btnNewButton_8.setIcon(null);
+			btnNewButton_8.setEnabled(true);
+		}
 	
 
 }
